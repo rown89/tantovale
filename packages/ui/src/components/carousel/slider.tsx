27e23e8817717b8ad0e7e013844bb6783e12bb39
rown89 <info@danilomongelli.it>
2025-04-11 20:37:16 +0200

    feat(ui): remove thumbs from slider component

--- packages/ui/src/components/carousel/slider.tsx
+++ packages/ui/src/components/carousel/slider.tsx
@@ -1,5 +1,5 @@
-import React, { ReactNode, useEffect, RefObject } from 'react';
-import { useKeenSlider, KeenSliderPlugin, KeenSliderInstance } from 'keen-slider/react';
+import React, { ReactNode, useEffect, RefObject, useState } from 'react';
+import { useKeenSlider } from 'keen-slider/react';
 
 import 'keen-slider/keen-slider.min.css';
 import './slider.css';
@@ -9,98 +9,73 @@ interface SliderProps {
 	thumbnails: ReactNode[];
 }
 
-function ThumbnailPlugin(mainRef: RefObject<KeenSliderInstance | null>): KeenSliderPlugin {
-	return (slider) => {
-		function removeActive() {
-			slider.slides.forEach((slide) => slide.classList.remove('active'));
-		}
-
-		function addActive(idx: number) {
-			if (slider.slides[idx]) {
-				slider.slides[idx].classList.add('active');
-			}
-		}
-
-		function addClickEvents() {
-			slider.slides.forEach((slide, idx) => {
-				slide.onclick = () => {
-					if (mainRef.current) mainRef.current.moveToIdx(idx);
-				};
-			});
-		}
-
-		slider.on('created', () => {
-			if (!mainRef.current) return;
-
-			if (slider.track.details) {
-				addActive(slider.track.details.rel);
-			}
-
-			addClickEvents();
-			mainRef.current.on('animationStarted', (main) => {
-				removeActive();
-				const next = main.animator.targetIdx || 0;
-
-				if (main.track.details) {
-					addActive(main.track.absToRel(next));
-					slider.moveToIdx(Math.min(slider.track.details.maxIdx || 0, next));
-				}
-			});
-		});
-
-		slider.on('updated', () => {
-			addClickEvents();
-		});
-	};
-}
-
 export default function Slider({ images, thumbnails }: SliderProps) {
+	const [currentSlide, setCurrentSlide] = React.useState(0);
+	const [loaded, setLoaded] = useState(false);
+
 	const [sliderRef, instanceRef] = useKeenSlider<HTMLDivElement>({
 		initial: 0,
-	});
-
-	const [thumbnailRef, thumbnailInstanceRef] = useKeenSlider<HTMLDivElement>(
-		{
-			initial: 0,
-			slides: {
-				perView: 4,
-				spacing: 10,
-			},
+		slideChanged(slider) {
+			setCurrentSlide(slider.track.details.rel);
 		},
-		[ThumbnailPlugin(instanceRef)],
-	);
+		created() {
+			setLoaded(true);
+		},
+	});
 
 	// Reinitialize slider on images update
 	useEffect(() => {
 		if (images.length > 0) {
 			setTimeout(() => {
 				instanceRef.current?.update();
-				thumbnailInstanceRef.current?.update();
 			}, 100); // Small delay to ensure DOM is updated
 		}
-	}, [images, instanceRef, thumbnailInstanceRef]);
+	}, [images, instanceRef]);
 
 	return (
 		<>
-			{images.length > 0 && (
+			<div className='navigation-wrapper min-h-[inherit]'>
 				<div ref={sliderRef} className='keen-slider min-h-[inherit]'>
-					{images.map((image, index) => (
-						<div key={index} className='keen-slider__slide'>
-							{image}
-						</div>
-					))}
+					{images.length > 0 && (
+						<>
+							{images.map((image, index) => (
+								<div key={index} className='keen-slider__slide'>
+									{image}
+								</div>
+							))}
+						</>
+					)}
 				</div>
-			)}
+			</div>
 
-			{images.length > 0 && (
-				<div ref={thumbnailRef} className='keen-slider thumbnail'>
-					{thumbnails.map((thumb, index) => (
-						<div key={index} className='keen-slider__slide'>
-							{thumb}
-						</div>
-					))}
+			{loaded && instanceRef.current && (
+				<div className='dots'>
+					{[...Array(instanceRef.current.track.details.slides.length).keys()].map((idx) => {
+						return (
+							<button
+								key={idx}
+								onClick={() => {
+									instanceRef.current?.moveToIdx(idx);
+								}}
+								className={'dot' + (currentSlide === idx ? ' active' : '')}></button>
+						);
+					})}
 				</div>
 			)}
 		</>
+	);
+}
+
+function Arrow(props: { disabled: boolean; left?: boolean; onClick: (e: any) => void }) {
+	const disabled = props.disabled ? ' arrow--disabled' : '';
+	return (
+		<svg
+			onClick={props.onClick}
+			className={`arrow ${props.left ? 'arrow--left' : 'arrow--right'} ${disabled}`}
+			xmlns='http://www.w3.org/2000/svg'
+			viewBox='0 0 24 24'>
+			{props.left && <path d='M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z' />}
+			{!props.left && <path d='M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z' />}
+		</svg>
 	);
 }
