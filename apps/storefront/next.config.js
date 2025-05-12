a07925be82bc289ad443c2d6742d79bddd8d42b9
rown89 <info@danilomongelli.it>
2025-05-12 12:21:43 +0200

    fix(storefront): remove custom headers

--- apps/storefront/next.config.js
+++ apps/storefront/next.config.js
@@ -20,51 +20,6 @@ const nextConfig = {
       },
     ];
   },
-  async headers() {
-    return [
-      /* {
-        // Apply these headers to all routes
-        source: "/(.*)",
-        headers: [
-          // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
-          {
-            key: "X-Content-Type-Options",
-            value: "nosniff",
-          },
-          // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
-          {
-            key: "X-Frame-Options",
-            value: "DENY",
-          },
-          // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-XSS-Protection
-          {
-            key: "Referrer-Policy",
-            value: "strict-origin-when-cross-origin",
-          },
-        ],
-      }, */
-      {
-        source: "/sw.js",
-        headers: [
-          {
-            // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Type
-            key: "Content-Type",
-            value: "application/javascript; charset=utf-8",
-          },
-          {
-            // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Cache-Control
-            key: "Cache-Control",
-            value: "no-cache, no-store, must-revalidate",
-          },
-          {
-            // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Pragma
-            key: "Content-Security-Policy",
-            value: "default-src 'self'; script-src 'self'",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
